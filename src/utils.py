91224f463da2bd81efda2d2d4b3ce52dd42dfcb7
Kourosh <Kourosh.ilchi@gmail.com>
2026-06-16 15:30:57 +0330

    adding Precision vs recall function

--- src/utils.py
+++ src/utils.py
@@ -1,5 +1,6 @@
 from sklearn.metrics import roc_curve
 import matplotlib.pyplot as plt
+from sklearn.metrics import precision_recall_curve
 
 def plot_roc_curve(y_true , y_pred , label=None):
     fpr, tpr, thresholds = roc_curve(y_true , y_pred)
@@ -10,6 +11,16 @@ def plot_roc_curve(y_true , y_pred , label=None):
     plt.axis([0, 1, 0, 1])
     plt.xlabel('False Positive Rate')
     plt.ylabel('True Positive Rate')
-    plt.legend()
-    plot_roc_curve(fpr, tpr)
+    plt.legend(loc="upper left")
+    plt.show()
+
+
+def plot_precision_vs_recall(y_true, y_pred):
+    precisions, recalls, thresholds = precision_recall_curve(y_true, y_pred)
+    plt.figure(figsize=(18,7))
+    plt.plot(recalls[:-1], precisions[:-1], "b-", label="Precision")
+    plt.xlabel("Recall")
+    plt.ylabel("Precision")
+    plt.legend(loc="upper left")
+    plt.ylim([0, 1])
     plt.show()
